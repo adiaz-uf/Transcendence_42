4d9725430a81a51d5060e34fde818f96b1e81930
adiaz-uf <adiazufano13@gmail.com>
2025-01-28 12:04:41 +0100

    First commit

---
+++ front/src/App.js
@@ -0,0 +1,16 @@
+import Login from './Login'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to trascendence with react
+        </p>
+        <Login />
+      </header>
+    </div>
+  );
+}
+
+export default App;
