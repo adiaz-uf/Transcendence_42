468654147a9dd1375eb58011c6aa17c1e7cc3c46
adiaz-uf <adiazufano13@gmail.com>
2025-02-27 12:11:31 +0100

    New: database tables Match & Tournament views

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,22 +7,25 @@ class Note(models.Model):
     create_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
 
-    def __str__(seld):
+    def __str__(self):
         return self.title
-
-""" class Tournament(models.Model):
-    date = models.DateTimeField(auto_now_add=True)
-    Semifinal1 = models.ForeignKey("Match", on_delete=models.SET_NULL, null=True)
-    Semifinal2 = models.ForeignKey("Match", on_delete=models.SET_NULL, null=True)
-    FinalMatch = models.ForeignKey("Match", on_delete=models.SET_NULL, null=True)
 
 class Match(models.Model):
     date = models.DateTimeField(auto_now_add=True)
-    Player1 = models.ForeignKey("User", on_delete=models.SET_NULL, null=True)
-    Player2 = models.ForeignKey("User", on_delete=models.SET_NULL, null=True)
+    Player1 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="matches_as_player1")
+    Player2 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="matches_as_player2")
     P1score = models.IntegerField()
-    P2score = models.IntegerField() """
+    P2score = models.IntegerField()
 
+    def __str__(self):
+        return f"Match between {self.Player1} and {self.Player2}"
 
+class Tournament(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    Semifinal1 = models.ForeignKey(Match, on_delete=models.SET_NULL, null=True, related_name="tournaments_as_semifinal1")
+    Semifinal2 = models.ForeignKey(Match, on_delete=models.SET_NULL, null=True, related_name="tournaments_as_semifinal2")
+    FinalMatch = models.ForeignKey(Match, on_delete=models.SET_NULL, null=True, related_name="tournaments_as_finalmatch")
 
-# Create your models here.
+    def __str__(self):
+        return f"Tournament on {self.date}"
+
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("notes/", views.NoteListCreate.as_view(), name="note-list"),
     path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
+    path("tournament/", views.CreateTournamentView.as_view(), name="tournament"),
+    path("match/", views.CreateMatchView.as_view(), name="match"),
 
 ]
