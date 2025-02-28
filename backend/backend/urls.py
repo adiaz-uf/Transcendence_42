8f499637426b41773ad8431b37a0ea62c9e3ec5d
aude-la- <aude-la-@student.42madrid.com>
2025-02-28 20:00:51 +0100

    FIX: add some game logic in server-side

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from api.views import CreateUserView, CreateTournamentView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from backend.websocket_consumers import PongConsumer
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +12,5 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include("api.urls")),
+    path("ws/game/", PongConsumer.as_asgi()),
 ]
