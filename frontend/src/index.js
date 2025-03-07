fb5b2125c00f756a47b86bc555bafc883b296dd1
aude-la- <aude-la-@student.42madrid.com>
2025-03-07 13:37:39 +0100

    FEAT: infra V3 I improved the build and up efficency as never before

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import RouteSwitch from "./components/RouteSwitch"
 import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import socket from './websocket'; // Import WebSocket initialization
 
 //import reportWebVitals from '../test/reportWebVitals';
 
