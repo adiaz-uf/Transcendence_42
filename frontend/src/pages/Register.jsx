8b9a56a6bdb50631d2ab4c327704cdb8499d4c91
aude-la- <aude-la-@student.42madrid.com>
2025-04-04 14:43:32 +0200

    Added a clean management for errors with the error box.

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -24,17 +24,24 @@ export default function Register({route}) {
         e.preventDefault();
         try {
             if (username === "" || email === "" || given_name === "" || surname === "" || password === "" || repeatpassword === "")
-                setError("Please enter all the fields");
-                setMessageType("info");
+                throw new Error("Please enter all the fields");
             if (password !== repeatpassword)
-                setError("Passwords aren't matching");
-                setMessageType("info");
+                throw new Error("Passwords aren't matching");
 
             await api.post(route, { username, password, email, given_name, surname });
             navigate("/login");
-        } catch (error) {
-            setError(error.response?.data?.detail || error.message);
+        } catch (err) {
+            if (err.response && err.response.data) {
+                const djangoErrors = err.response.data;
+                const errorMessages = Object.entries(djangoErrors)
+                    .map(([field, messages]) => `${field}: ${messages.join(", ")}`)
+                    .join("\n");
+                setError(errorMessages);
+            } else {
+                setError(err.message || "An error occurred");
+            }
             setMessageType("error");
+    
         } finally {
             setLoading(false);
         }
