6e76e4f8860066fce4af600b209474bbc24f2747
aude-la- <aude-la-@student.42madrid.com>
2025-04-04 17:38:55 +0200

    FIX: added a first plan error box for change data and manage the username update in the navbar without reload all the page

--- frontend/src/components/navigation/Navbar.js
+++ frontend/src/components/navigation/Navbar.js
@@ -1,47 +1,29 @@
 import { Nav, Navbar, Container } from "react-bootstrap";
-import { useState, useEffect } from "react";
-import '../../styles/App.css'
+import '../../styles/App.css';
 
-export default function NavBar() {
-    const [username, setUsername] = useState('');
-
-    useEffect(() => {
-    }, []); 
-    
-    
-    useEffect(() => {
-        const handleStorageChange = () => {
-            const savedUsername = localStorage.getItem("username");
-            setUsername(savedUsername || 'Guest');
-        };
-
-        window.addEventListener('storage', handleStorageChange);
-        handleStorageChange();
-        return () => window.removeEventListener('storage', handleStorageChange);
-    }, []);
-
+export default function NavBar({ username }) {
     return (
         <div tabIndex="0">
-        <Navbar expand="lg" bg="dark" variant="dark" className="navbar-fixed-top">
-            <Container>
-                <Navbar.Brand href="/">Transcendence</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-bar" />
-                <Navbar.Collapse id="responsive-navbar-bar">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/profile">Profile</Nav.Link>
-                        <Nav.Link href="/tournament">Tournament</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Navbar.Text>
-                            Signed in as: 
-                            <a href="/profile" className="user-link"> {username}</a> 
-                            <a href="/logout" className="logout-link"> Logout </a>
-                        </Navbar.Text>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+            <Navbar expand="lg" bg="dark" variant="dark" className="navbar-fixed-top">
+                <Container>
+                    <Navbar.Brand href="/">Transcendence</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-bar" />
+                    <Navbar.Collapse id="responsive-navbar-bar">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/profile">Profile</Nav.Link>
+                            <Nav.Link href="/tournament">Tournament</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Navbar.Text>
+                                Signed in as: 
+                                <a href="/profile" className="user-link"> {username}</a> 
+                                <a href="/logout" className="logout-link"> Logout </a>
+                            </Navbar.Text>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 }
