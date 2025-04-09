af9264d82906907098a1e6ef2b16694744f90f1c
root <root@localhost.42madrid.com>
2025-04-09 17:23:11 +0200

    Menu Rearrange - redundant code deletion

--- frontend/src/components/navigation/Menu.jsx
+++ frontend/src/components/navigation/Menu.jsx
@@ -9,9 +9,8 @@ import GameOverModal from "../GameOverModal";
 export const Menu = () => {
   const { setGameMode, setIsMultiplayer, setPlayerType } = useGameSetting();
 
-  const [OnlineButtons, setOnlineButtons] = useState(false);
-  const [InvitationModal, setInvationBool] = useState(false);
-  const [InvitationModal2, setInvationBool2] = useState(false); //TODO: delete, just for test modal
+  const [TestVar1, setInvationBool] = useState(false);
+  const [TestVar, setInvationBool2] = useState(false); //TODO: delete, just for test modal
   const [gameType, setGameType] = useState(null);  // "match | tournament" *TODO: Join to context??
 
   const navigate = useNavigate();
@@ -22,77 +21,43 @@ export const Menu = () => {
         setIsMultiplayer(false);
         setInvationBool(true);
         setGameMode("local");
-        /* navigate("/local"); */
-      // let response = await POSTcreateMatch({
-      //   'is_multiplayer':Ismultiplayer,
-      //   'is_started':IsStarted,
-      //   'left_score':left_score,
-      //   'right_score':right_score, 
-      //   'player_left_username':localStorage.getItem('userId'),
-      //   'player_right_username': right_username
-      // });//temporal 
-      // console.log(response);
-      //setMatchId(response['id']);
-
-      // Pasar por Invite Modal
-
-      // Pasar por send.msg(connectToMatch) Y send.msg(game_active)
-
-
-
-      //navigate("/game");
-    } else if (mode === "online") {
-      setOnlineButtons(true);
-    } else if (mode === "online-create") {
-      setGameMode("online-create");
-      setIsMultiplayer(true);
-      setPlayerType("host");
-      navigate("/invite");
-    } else if (mode === "online-join") {
+    }
+    else if (mode === "online-join") {
       setGameMode("online-join");
       setIsMultiplayer(true);
       setPlayerType("guest");
       navigate("/join");
     }
-    else if (mode === "tournament-start") { 
-      setGameMode("local"); // TODO
-      setIsMultiplayer(false); // TODO
-      setGameType("tournament");
-      setInvationBool(true);
+    else if (mode === "tournament test buttone delete me after") { 
+
     }
-    else if (mode === "tournament-join") {
+    else if (mode === "test gameover modal") {
 
       setInvationBool2(true);
     }
   };
   return (
     <div className="menu-container">
-      <h1>Select Game Mode</h1>
-      { !OnlineButtons? (
-        <>
-        <Button className="m-5" onClick={() => handleSelectMode("local")}>Local Game (2P)</Button>
-        <Button className="m-5" onClick={() => handleSelectMode("online")}>Online Game</Button>
-        </>)
-        :
-        (<>
-        <Button className="m-5 btn-warning" onClick={() => handleSelectMode("online-create")}>Create Game</Button>
-        <Button className="m-5 btn-warning" onClick={() => handleSelectMode("online-join")}>Join Game</Button>
-        </>)
-      }
-      <h1>Or start a Tournament!</h1>
+      <h1>Do you want a local game or a tournament?!</h1>
+      <Button className="m-5" onClick={() => handleSelectMode("local")}>Local Game (2P)</Button>
       <Button className="m-5 btn-info" onClick={() => handleSelectMode("local")}>Start Tournament</Button>
-      <Button className="m-5 btn-info" onClick={() => handleSelectMode("tournament-join")}>Join Tournament</Button>{/* TODO: Join tournament Logic?*/}
       <h5 className="tournament-info">
         Our 4-player tournaments are designed for players to compete in a series of matches, 
-        with the top two advancing to a final. The inviter must enter three usernames to set up the tournament, 
-        and the other three players must join to complete the competition. 
-        This ensures a structured and fair format for skill-based play.
+        with the top two advancing to a final. The inviter must enter three usernames to set up the tournament.
       </h5>
-      { InvitationModal && (
-        <InvitePlayer showModal={InvitationModal} handleCloseModal={()=>{setInvationBool(false)}} gameType={gameType}/>
+
+      {/* TODO: DELETEME*/}
+      <Button className="m-5" onClick={() => handleSelectMode("tournament test buttone delete me after")}>Test Button </Button>
+      <Button className="m-5 btn-info" onClick={() => handleSelectMode("test gameover modal")}>Also a Test Button</Button>
+
+
+
+
+      { TestVar1 && (
+        <InvitePlayer showModal={TestVar1} handleCloseModal={()=>{setInvationBool(false)}} gameType={gameType}/>
       )}
-      { InvitationModal2 && ( //TODO: delete this modal here, just for test 
-        <GameOverModal showModal={InvitationModal2} handleCloseModal={()=>{setInvationBool2(false)}} player1={"Alex"} player2={"suuu"} score1={10} score2={5}/>
+      { TestVar && ( //TODO: delete this modal here, just for test 
+        <GameOverModal showModal={TestVar} handleCloseModal={()=>{setInvationBool2(false)}} player1={"Alex"} player2={"suuu"} score1={10} score2={5}/>
       )}
     </div>
   );
