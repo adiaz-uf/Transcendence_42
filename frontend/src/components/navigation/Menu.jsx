adb1de2699ab3506b0331e3c22d5e21ea6b2ea9f
aude-la- <aude-la-@student.42madrid.com>
2025-04-10 14:27:38 +0200

    FIX: change the menu buttons with Alex's version.

--- frontend/src/components/navigation/Menu.jsx
+++ frontend/src/components/navigation/Menu.jsx
@@ -13,11 +13,6 @@ export const Menu = () => {
   const [gameType, setGameType] = useState(null);  // "match | tournament" *TODO: Join to context??
 
   const navigate = useNavigate();
-
-
-
-
-
   
   const handleSelectMode = async (mode) => {
     if (mode === "local") {
@@ -47,16 +42,12 @@ export const Menu = () => {
     }
   };
 
-
-
-
-
-
   return (
     <div className="menu-container">
-      <h1>Do you want a 2 player game or a tournament?!</h1>
-      <Button className="m-5" onClick={() => handleSelectMode("local")}>2 Player Game</Button>
-      <Button className="m-5 btn-info" onClick={() => handleSelectMode("tournament")}>Tournament Logic Attempt</Button>
+      <h1>Play Single Game</h1>
+      <Button className="m-5" onClick={() => handleSelectMode("local")}>Start Game (2P)</Button>
+      <h1>Or start a Tournament!</h1>
+      <Button className="m-5 btn-info" onClick={() => handleSelectMode("tournament")}>Start Tournament</Button>
       <h5 className="tournament-info">
         Our 4-player tournaments are designed for players to compete in a series of matches, 
         with the top two advancing to a final. The inviter must enter three usernames to set up the tournament.
