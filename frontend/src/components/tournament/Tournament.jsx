d2dffd8450192d7ab39a6c4c32d6734520ea5ffa
SanPy <santi.magniny@gmail.com>
2025-04-11 01:41:52 +0200

    WIP Tournament UI

--- frontend/src/components/tournament/Tournament.jsx
+++ frontend/src/components/tournament/Tournament.jsx
@@ -1,98 +1,97 @@
-import { useTournamentSetting } from '../contexts/TournamentContext';
-import {GETCheckUsernameExists, POSTcreateMatch, POSTcreateTournament, PATCHAddMatchToTournament, GETTournamentDetails, PATCHAddWinnerToTournament} from "../api-consumer/fetch";
-import { useGameSetting } from '../contexts/GameContext';
-import React, { useState } from 'react';
-import { Button } from 'react-bootstrap';
-import GameApp from '../game/GameApp';
-import {useNavigate} from "react-router-dom";
+// import { useTournamentSetting } from '../contexts/TournamentContext';
+// import {GETCheckUsernameExists, POSTcreateMatch, POSTcreateTournament, PATCHAddMatchToTournament, GETTournamentDetails, PATCHAddWinnerToTournament} from "../api-consumer/fetch";
+// import { useGameSetting } from '../contexts/GameContext';
+// import React, { useState } from 'react';
+// import { Button } from 'react-bootstrap';
+// import GameApp from '../game/GameApp';
+// import {useNavigate} from "react-router-dom";
 
 
 
+// const Tournament =  () =>
+//     {
+//         const {tournamentId, 
+//             Player1, Player2, Player3, Player4,
+//             Player1username, Player2username, Player3username, Player4username} = useTournamentSetting();                   //de momento necessario porque no se si puede cambiar la orden de accesso de backend
+//         const {} = useTournamentSetting();                   //de momento necessario porque no se si puede cambiar la orden de accesso de backend
+//         const [winner1, setwinner1] = useState("");
+//         const [winner2, setwinner2] = useState("");
+//         const [winner3, setwinner3] = useState("");
+//         const navigate = useNavigate(); 
 
-const Tournament =  () =>
-    {
-        const {tournamentId, 
-            Player1, Player2, Player3, Player4,
-            Player1username, Player2username, Player3username, Player4username} = useTournamentSetting();                   //de momento necessario porque no se si puede cambiar la orden de accesso de backend
-        const {} = useTournamentSetting();                   //de momento necessario porque no se si puede cambiar la orden de accesso de backend
-        const [winner1, setwinner1] = useState("");
-        const [winner2, setwinner2] = useState("");
-        const [winner3, setwinner3] = useState("");
-        const navigate = useNavigate(); 
+//         const post_matches_start = async (p1, p2) => {
+//             // BTW we post macthes at creation for allowing internal handling through websocket or simple patches during game, not at end. The end is not a creation, for better arch it is an update
+//             console.log(`POST_matches_start(): p1 ${p1} p2 ${p2}`);
+//             let payload1 = {
+//                 player_left: p1,
+//                 player_right: p2,
+//                 is_multiplayer: true,
+//                 left_score: 0,
+//                 right_score: 0,
+//                 is_started: false,
+//                 };
+//             console.log(`Payload Created: ${payload}`);
+//             const LocalMatchResponse = await POSTcreateMatch(payload1);
+//             console.log(`POST response: ${LocalMatchResponse}`);
+//         };
 
-        const game_handler = async () => {
-            if (winner1 == ""){
-                console.log("Entered");
-                let payload1 = {
-                    player_left: Player1,
-                    player_right: Player2,
-                    is_multiplayer: true,
-                    left_score: 0,
-                    right_score: 0,
-                    is_started: false,
-                    };
-                console.log("Payload Created");
-                const LocalMatchResponse = await POSTcreateMatch(payload1);
-                console.log("POST");
-                  navigate("/local")
-
-            }else if (winner2 == ""){
-
-            }  
-            else if (winner3 == ""){
-
-            }
-        };
-
-        game_handler()
-
-        return(
-            <>
-            </>
-        );
+//         return(
+//             <>
+//             </>
+//         );
+/// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   
-            // [x] Get usernames
+//             // [x] Get usernames
             
-            // [] Launch match one (needs to be slightly different than 
-            // the game logic used in the local game as we need to not got to menu when it ends and reload for the next game)
+//             // [] Launch match one (needs to be slightly different than 
+                    //[santi] make a generic function to posting matchCreation   AND append a match to Tournaments
+                
+//             //the game logic used in the local game as we need to not got to menu when it ends and reload for the next game)
+                // [santi] lets call the component directly instead of navigate
+                
+//             // [] set winner1 to match winner
+                    //[Santi] Use effect    for winners Of each finals
+//                //const TournamentResponse2 =  await PATCHAddWinnerToTournament(TournamentResponse4.id, player_3.userProfile.id);
 
-            // [] set winner1 to match winner
-                        //const TournamentResponse2 =  await PATCHAddWinnerToTournament(TournamentResponse4.id, player_3.userProfile.id);
+//             // [] POST match     // [Santi Initial post function]
+//                         //let payload1 = {
+//                         //    player_left: player_1_id,
+//                         //    player_right: player_2.id,
+//                         //    is_multiplayer: true,
+//                         //    left_score: 0,
+//                         //    right_score: 0,
+//                         //    is_started: false,
+//                         //  };
+//                         //  const LocalMatchResponse = await POSTcreateMatch(payload1);
 
-            // [] POST match
-                        //let payload1 = {
-                        //    player_left: player_1_id,
-                        //    player_right: player_2.id,
-                        //    is_multiplayer: true,
-                        //    left_score: 0,
-                        //    right_score: 0,
-                        //    is_started: false,
-                        //  };
-                        //  const LocalMatchResponse = await POSTcreateMatch(payload1);
-
-            // [] PATCH add match to tournament
-                        //const result = await PATCHAddMatchToTournament(tournament.id, match.id);
-
+//             // [] PATCH add match to tournament
+//                         //const result = await PATCHAddMatchToTournament(tournament.id, match.id);
+                // [Santi] directly after the post
+                // [Santi] laucnh <LocalGame/>
+                //                  Passs prop (param) to return to tourna
+                
            
 
-            // [] POST match
 
-            // [] PATCH add match to tournament
 
-            // [] Launch match two
+//             // [] POST match
 
-            // [] set winner2 to match winner
+//             // [] PATCH add match to tournament
 
-            // [] POST match
+//             // [] Launch match two
 
-            // [] PATCH add match to tournament
+//             // [] set winner2 to match winner
+
+//             // [] POST match
+
+//             // [] PATCH add match to tournament
               
-            // [] Launch final
+//             // [] Launch final
 
-            // [] PATCH set winner to be tournament winner.
-        }
-
-export default Tournament; 
+//             // [] PATCH set winner to be tournament winner.
+//         }
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// export default Tournament; 
 
 
 //            <Button className="m-5 btn-info" >Tourn ID {tournamentId}</Button>
