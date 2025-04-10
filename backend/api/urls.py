bb1b3f25ad07363a6b3ec9ec29ae221dd952e87a
SanPy <santi.magniny@gmail.com>
2025-04-10 03:04:21 +0200

    FEAT: Active JWT based session for friends

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,6 +6,7 @@ from api.views.user.views import CreateUserView, UserProfileView, LoginView, Che
 from api.views.match.views import MatchCreationView, CreateOnlineMatchView, UserMatchListView, MatchScoreUpdateView, AvailableMatchView
 from api.views.auth.TwoFA import Setup2FAView
 from api.views.auth.auth42 import FTAuthCallbackView
+from api.views.auth.jwt import CustomTokenRefreshView
 from api.views.game.cli_views import GameStateView, GameControlView, PlayerMoveView, GameStatsView
 from api.views.tournament.views import CreateTournamentView, AddMatchToTournamentView, AddWinnerToTournamentView, UserTournWinnerCountView
 
@@ -14,7 +15,8 @@ urlpatterns = [
     path('user/login/', LoginView.as_view(), name='login'), #POST #ANY
     path('user/register/', CreateUserView.as_view(), name='register'), #POST #ANY
     path('user/profile/', UserProfileView.as_view(), name='profile'), #UPDT & GET #AUTH
-    path('user/refresh/', TokenRefreshView.as_view(), name='refresh'), #GET? #AUTH 
+    
+    path('user/refresh/', CustomTokenRefreshView.as_view(), name='refresh'), #GET? #AUTH 
     
     path('user/profile/<str:username>', OthersProfileView.as_view(), name='profile'), #UPDT & GET #AUTH
     path('user/exists/<str:username>', CheckUserExistsView.as_view(), name='check_username'), #GET #AUTH
